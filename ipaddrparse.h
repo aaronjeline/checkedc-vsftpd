0aaa7c7b44beedaa59dda6fe454e9c7b1e141433
John Kastner <john@correctcomputation.com>
2021-06-14 13:52:32 -0400

    Fully checked ipaddrparse.c

--- ipaddrparse.h
+++ ipaddrparse.h
@@ -8,11 +8,11 @@ struct mystr;
  * and safety.
  */
 
-const unsigned char *vsf_sysutil_parse_ipv6(const struct mystr *p_str : itype(_Ptr<const struct mystr>)) : itype(_Ptr<const unsigned char>);
+const unsigned char *vsf_sysutil_parse_ipv6(const struct mystr *p_str : itype(_Ptr<const struct mystr>)) : itype(_Nt_array_ptr<const unsigned char>);
 
-const unsigned char *vsf_sysutil_parse_ipv4(const struct mystr *p_str : itype(_Ptr<const struct mystr>)) : itype(_Ptr<const unsigned char>);
+const unsigned char *vsf_sysutil_parse_ipv4(const struct mystr *p_str : itype(_Ptr<const struct mystr>)) : itype(_Array_ptr<const unsigned char>) count(4);
 
-const unsigned char *vsf_sysutil_parse_uchar_string_sep(const struct mystr *p_str : itype(_Ptr<const struct mystr>), char sep, unsigned char *p_items : itype(_Array_ptr<unsigned char>) count(items), unsigned int items) : itype(_Ptr<const unsigned char>);
+const unsigned char *vsf_sysutil_parse_uchar_string_sep(const struct mystr *p_str : itype(_Ptr<const struct mystr>), char sep, unsigned char *p_items : itype(_Array_ptr<unsigned char>) count(items), unsigned int items) : itype(_Array_ptr<const unsigned char>) count(items);
 
 #endif /* VSF_IPADDRPARSE_H */
 
